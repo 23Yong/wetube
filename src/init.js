90fd59251b9f226bfca964cd574cab0c9f205196
Jeong Yong <jy1239824@gmail.com>
2021-06-12 23:09:17 +0900

    query practice

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";  // connecting to Mongo
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log(`✅Server listening on port http://localhost:${PORT}🚀`);
+
+app.listen(PORT, handleListening);    // callback은 기본적으로 서버가 시작될 때 작동하는 함수
--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
-import "./db";  // connecting to Mongo
 import express from "express";
 import morgan from "morgan";    // morgan은 node.js 용 request logger middleware
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();  // create express application
 const logger = morgan("dev"); // morgan함수는 middleware를 return, next()를 가지고 있음.
@@ -18,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);    // 누군가가 "/videos"로 시작하는 url에 접근하면 videoRouter에 있는 controller를 찾게함.
 app.use("/users", userRouter);
 
-const handleListening = () => 
-    console.log(`✅Server listening on port http://localhost:${PORT}🚀`);
-
-app.listen(PORT, handleListening);    // callback은 기본적으로 서버가 시작될 때 작동하는 함수
+export default app;
