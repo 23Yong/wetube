240763facd694677dfe31048a3bf95ce1f4e294f
Jeong Yong <jy1239824@gmail.com>
2021-06-14 23:14:11 +0900

    video detail

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getUpload, postUpload, getEdit, postEdit } from "../controllers/
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); // 숫자만 받아오도록
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch); // 숫자만 받아오도록
+videoRouter.route("/:id([0-9a-f]{24)}/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
