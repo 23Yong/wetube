3062779e43bc647eff6a583c044b22e77dee7423
Jeong Yong <jy1239824@gmail.com>
2021-06-16 15:08:12 +0900

    delete and search video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,13 +4,15 @@ import {
     getUpload,
     postUpload,
     getEdit,
-    postEdit
+    postEdit,
+    deleteVideo
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch); // 숫자만 받아오도록
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
