966c7cede9f8e9233ecc17d061a26584c7efb7a0
23Yong <66981851+23Yong@users.noreply.github.com>
2021-07-11 19:50:11 +0900

    make videoPlayer_1

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,56 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+
+const handlePlayClick = (event) => {
+    if(video.paused) {
+        playBtn.innerTExt = "Pause";
+        video.play();
+    } else {
+        video.pause();
+    }
+    playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMute = (event) => {
+    if(video.muted){
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+    const { target: { value } } = event;
+    if(video.muted) {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+};
+
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5);
+
+const handleLoadedMetaData = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
