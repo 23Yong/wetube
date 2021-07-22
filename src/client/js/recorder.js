fb5d08de47a0050b42ee0e26d01c0176577bc18a
23Yong <66981851+23Yong@users.noreply.github.com>
2021-07-22 22:38:58 +0900

    transcode video (ffmpeg)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import  { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,7 +7,15 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async() => {
+    const ffmpeg = createFFmpeg({ log: true });
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile)); // ffmpeg의 가상의 세계에 파일을 생성
+
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // ffmpeg.run은 가상 컴퓨터에 이미 존재하는 파일을 i(input)으로 받는것
+        // 초당 60프레임으로 인코딩
+    
     const a = document.createElement("a");
     a.href = videoFile;
     a.download = "MyRecording.webm";
